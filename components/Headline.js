ef95cfe25eb944fc6ea67884c7989053b09db92d
yamashin01 <syf98043@gmail.com>
2021-04-07 21:55:42 +0900

    ♻️ ヘッドライン部分をコンポーネント化

---
+++ components/Headline.js
@@ -0,0 +1,16 @@
+import React from "react"
+import styles from '../styles/Home.module.css'
+
+export default function Headline(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1 className={styles.title}>{props.page} Page</h1>
+
+      <p className={styles.description}>
+        Get started by editing{' '}
+        <code className={styles.code}>pages/{props.page}.js</code>
+      </p>
+    </div>
+  )
+}
