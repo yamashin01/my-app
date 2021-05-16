7a5e5283965272ef0c0cc89eb79a601532d4eeec
yamashin01 <syf98043@gmail.com>
2021-05-16 11:18:54 +0900

    ⚡️ useCallback, useEffectの第２引数に関して説明

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,21 +3,20 @@ import styles from 'src/styles/Home.module.css'
 import {Footer} from "src/components/Footer"
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(1); // 分割代入
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
   
   useEffect(() => {
-    console.log("マウント時");
     document.body.style.backgroundColor = "lightBlue";
     return () => {
-      console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
   }, []);
