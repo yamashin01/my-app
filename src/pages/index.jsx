67cd1837c39b3203ad324d7ff0c203199a9fce13
yamashin01 <syf98043@gmail.com>
2021-05-24 21:28:55 +0900

    ✨ useStateで配列を扱った

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [count, setCount] = useState(1); // 分割代入
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -27,7 +28,7 @@ export default function Home() {
   const handleDisplay = useCallback(() => {
     setIsShow((prevIsShow) => !prevIsShow);
   }, []);
-  
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightBlue";
     return () => {
@@ -35,6 +36,15 @@ export default function Home() {
     };
   }, []);
   
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some(item => item === text)) {
+        alert("同じ要素が既に存在します");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
 
   return (
     <div className={styles.container}>
@@ -48,6 +58,14 @@ export default function Home() {
         {isShow ? "非表示" : "表示"}
       </button>
       <input type="text" value={text} onChange={handleChange}/>
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map(item=> {
+          return (
+            <li key={item}>{item}</li>
+          )
+        })}
+      </ul>
       <Main page = "index" />
 
       <Footer />
